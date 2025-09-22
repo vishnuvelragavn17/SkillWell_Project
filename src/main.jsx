50dbfe3e0f7829c299df708b728e955b8e95a25f
Vishnuvel <Vishnuvelragavan17@gmail.com>
2025-09-22 21:41:03 +0530

    main.jsx changes code

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/SkillWell_Project">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
