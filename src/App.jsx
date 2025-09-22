7c2286f17a87284eca86c8ec4a9a96bf33f8479b
Vishnuvel <Vishnuvelragavan17@gmail.com>
2025-09-22 21:15:27 +0530

    project code

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './Components/HomePage';
+import AboutPage from './Components/About';
+import LoginPage from './Components/LoginPage';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+
+  );
+}
+
+export default App;
